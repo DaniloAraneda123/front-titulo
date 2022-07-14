84ffce96e9bb1a9883958d9ffbba416d6553e1cd
Danilo Esteban araneda <daes1128@gmail.com>
2022-07-14 19:06:23 -0400

    buttom help main regitros historicos

--- src/app/historicidad/components/help-multiple/help-multiple.component.ts
+++ src/app/historicidad/components/help-multiple/help-multiple.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-help-multiple',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelpMultipleComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<HelpMultipleComponent>
+  ) { }
 
   ngOnInit(): void {
   }
